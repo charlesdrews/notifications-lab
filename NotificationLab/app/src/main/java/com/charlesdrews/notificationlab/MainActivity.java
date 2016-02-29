db4392e94591713dbc69a0ad15dbc986fcd8fb1d
charlesdrews <charlesdrews@gmail.com>
2016-02-29 17:24:16 -0500

    Add make phone call notification

--- NotificationLab/app/src/main/java/com/charlesdrews/notificationlab/MainActivity.java
+++ NotificationLab/app/src/main/java/com/charlesdrews/notificationlab/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // launch a notification with a call action
+        NotificationCompat.Builder callBuilder = new NotificationCompat.Builder(this);
+        callBuilder.setSmallIcon(R.drawable.ic_phone_forwarded_white_18dp);
+        callBuilder.setContentTitle("Make a phone call");
+        callBuilder.setContentText("Call 555-555-5555");
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel: 5555555555"));
+
+        PendingIntent callPendingIntent = PendingIntent.getActivity(
+                this, (int) System.currentTimeMillis(), callIntent, 0);
+
+        //callBuilder.setContentIntent(callPendingIntent);
+        //callBuilder.setAutoCancel(false);
+        callBuilder.addAction(R.drawable.ic_phone_forwarded_white_18dp, "CALL NOW!", callPendingIntent);
+        NotificationManager callManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        callManager.notify(2, callBuilder.build());
+
+
+
+        // launch a notification showing network connectivity
         ConnectivityManager cmc = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cmc.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
@@ -37,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
         builder.setStyle(style);
 
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, (int) System.currentTimeMillis(), intent, 0);
 
         builder.setContentIntent(pendingIntent);
 
